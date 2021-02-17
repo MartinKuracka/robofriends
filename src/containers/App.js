2ab856b0686de6d8264337bcc5440ce312993409
martin kuracka <m.kuracka@gmail.com>
2021-02-17 12:53:05 +0100

    hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,46 +1,39 @@
-import React, { Component } from 'react'; 
+import React, { useState, useEffect } from 'react'; 
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 
-class App extends Component { 
-	  constructor() {
-	  	super()       
-	  	this.state = { 
-		robots: [], 
-		searchField: ''  
-	    }
-	  }
+function App (){ 
+	 const [robots,setRobots] = useState([]);
+	 const [searchfield, setSearchfield] = useState('');
+	 const [count, setCount] = useState(0);
 
-	  componentDidMount() {
+	  useEffect(() => {
 	  	fetch('https://jsonplaceholder.typicode.com/users') 
 	  		.then(response => response.json()) 
-	  		.then(users => {this.setState({robots: users})})
-	  }
+	  		.then(users => {setRobots(users)})
+	  		console.log(count)
+	  },[count]) // only run if count changes. if [] empty, runs only once on App function loading
 	  
-	  onSearchChange = (event) => {  
-	  	this.setState({searchField: event.target.value}); 
+	  const onSearchChange = (event) => {  
+	  	setSearchfield(event.target.value); 
 	  }
 
-	  render() {
-	  	const filteredRobots = this.state.robots.filter(robot => {
-	  		return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
-  	    })
-  	    if (this.state.robots.length === 0) {		
-  	    	return <h1 className="tc"> Loading </h1>
-  	    } else {  
-	      return (
-			<div className="tc">
+	  const filteredRobots = robots.filter(robot => {
+	 	return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+ 	  })
+  	return !robots.length 
+  		?	<h1 className="tc"> Loading... </h1>
+  	  : (
+		<div className="tc">
 				<h1>ROBOFRIENDS</h1>
-				<SearchBox searchChange={this.onSearchChange}/>
+				<button onClick={() => setCount(count+1)}>click me</button>				
+				<SearchBox searchChange={onSearchChange}/>
 				<Scroll>
 					<CardList robots={filteredRobots}/>  
 				</Scroll>
-			</div>
+		</div>
 	      );
-	    }
-      }
-
 }
 
 export default App;
